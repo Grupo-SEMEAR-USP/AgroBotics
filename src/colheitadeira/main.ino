84c0d46f1437e410bbfb92470781839489a731e9
XicoRMA <francisco.affonso02@gmail.com>
2021-05-01 16:27:02 -0300

    Update code
    Adicionando um modelo de utilização da biblioteca criada, adicionando as funções correspondentes ao controle do pwm.

--- src/colheitadeira/main.ino
+++ src/colheitadeira/main.ino
@@ -1,9 +1,37 @@
 #include "utils.h" //<utils.h>
 
+/*
+//Definição dos canais
+const int motorRightTop = ;
+const int motorRightBottom = ;
+const int motorLeftTop = ;
+const int motorLeftBottom = ;
+
+const int sensor1 = ;
+const int sensor2 = ;
+const int sensor3 = ;
+const int sensor4 = ;
+const int sensor5 = ;
+
+const int pwm0 = ;
+const int pwm1 = ;
+
+//Setup dos pinos
+Utils robot (
+    motorRightTop, motorRightBottom, motorLeftTop, motorLeftBottom,
+    pwm0, pwm1,
+    sensor1, sensor2, sensor3, sensor4, sensor5); 
+*/
+
 void setup() {
 
 }
 
 void loop() {
+    /* 
+    //Loop a função de seguir linha
 
+    robot.followLine (speed)
+
+    */
 }
